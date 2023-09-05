335d4d20b948e335930821ddf4bcfdf671373b5d
Fábio Sander Novaes Corrêa <fabiosandernt@gmail.com>
2023-09-05 20:14:46 -0300

    Add CofigurationModule

---
+++ Agenda.Application/ConfigurationModule.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Agenda.Application
+{
+    public static class ConfigurationModule
+    {
+        public static IServiceCollection RegisterApplication(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+
+            services.AddAutoMapper(typeof(Application.ConfigurationModule).Assembly);
+            //services.AddMediatR(typeof(Application.ConfigurationModule).Assembly);
+
+            //services.AddScoped<IUserService, UserService>();
+        
+
+            services.AddHttpClient();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                );
+            });
+
+            //services.AddAuthentication(x =>
+            //{
+            //    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            //}).AddJwtBearer(x =>
+            //{
+            //    x.TokenValidationParameters = new TokenValidationParameters
+            //    {
+            //        ValidateIssuerSigningKey = true,
+            //        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(configuration["JwtSecurity:SecurityKey"])),
+            //        ValidateIssuer = false,
+            //        ValidateAudience = false,
+            //        ValidateLifetime = true
+            //    };
+            //});
+
+            var info = new OpenApiInfo();
+            info.Version = "V1";
+            info.Title = "API Agenda";
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", info);
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Insira o token JWT desta maneira : Bearer {seu token}",
+                    Name = "Authorization",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In= ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                    }
+                });
+            });
+
+            return services;
+        }
+    }
+}
